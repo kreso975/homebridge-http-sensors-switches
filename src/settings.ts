d9b5283d54e5ba8ce2f37a0537ed4540f2503851
kreso975 <kresimir.kokanovic@gmail.com>
2024-08-24 00:10:24 +0200

    2nd

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HttpSensorsAndwitchesHomebridgePlugin';
+export const PLATFORM_NAME = 'HttpSensorsAndSwitches';
 
 /**
  * This must match the name of your plugin as defined the package.json
